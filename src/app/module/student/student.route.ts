c2a7eccc81b5cdffaa4674bcaa04c20958124e4b
abdus shobhan <shobhanrahman21@gmail.com>
2024-12-13 18:00:24 +0600

    last dev

---
+++ src/app/module/student/student.route.ts
@@ -0,0 +1,26 @@
+//
+import express, { Router } from "express";
+import { StudentControllers } from "./student.controller";
+
+const route = express.Router();
+
+// route.post("/", StudentControllers.createStudent);
+
+route.post("/", StudentControllers.createStudent);
+route.post("/orders", StudentControllers.orderStudent);
+
+route.get("/", StudentControllers.getAllStudents);
+
+route.get("/order", StudentControllers.getAllorders);
+// route.put("/:_id", StudentControllers.getUpdateStudents);
+route.put("/:_id", StudentControllers.getUpdateStudents);
+
+// find Onestudent by using studentID
+route.get("/:studentID", StudentControllers.getSingleStudents);
+
+// this is the route for delete user
+route.delete("/:studentID", StudentControllers.getDeleteeStudents);
+
+//getDeleteeStudents
+
+export const studentRoutes = route;
